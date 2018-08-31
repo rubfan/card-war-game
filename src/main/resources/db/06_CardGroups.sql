2cffefe5932705fceaf29e08caa6d96bdae6c4a8
YarotskiyAndrey <yrtsky@gmail.com>
2018-08-31 21:18:40 +0300

    06_CardGroups.sql and 07_CardImpacts.sql

---
+++ src/main/resources/db/06_CardGroups.sql
@@ -0,0 +1,6 @@
+INSERT INTO `Card_Group`
+    (`id`,`name`,`description`)
+VALUES
+		(1,'Build','Builds a building'),
+		(2,'Upgrade','Upgrades a building'),
+		(3,'Action','Performs various actions'),
---
+++ src/main/resources/db/07_CardImpacts.sql
@@ -0,0 +1,12 @@
+INSERT INTO `Card_Impact`
+    (`id`  AUTO_INCREMENT, `card_id``card_group_id`,
+    `p1_building_id`, `p2_building_id`,
+    `p1_building_number`, `p2_building_number`,
+    `p1_resource_id`, `p2_resource_id`,
+    `p1_resource_number`, `p2_resource_number`,
+    `p1_upgrade_id`, `p2_upgrade_id`,
+    `p1_upgrade_number`, `p2_upgrade_number`,
+    `necessary_building_id`, `necessary_upgrade_id`,
+    `necessary_building_number`, `necessary_upgrade_number`)
+VALUES
+    
