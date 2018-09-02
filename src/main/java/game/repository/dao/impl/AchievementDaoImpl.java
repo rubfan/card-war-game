37f306b353db05ae838c5add6a488b2c3229ae13
Alexey Kostash <odessakost@gmail.com>
2018-09-02 12:11:28 +0300

    add AchievementDto

--- src/main/java/game/repository/dao/impl/AchievementDaoImpl.java
+++ src/main/java/game/repository/dao/impl/AchievementDaoImpl.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AchievementDaoImpl implements AchievementDao {
@@ -18,13 +18,13 @@ public class AchievementDaoImpl implements AchievementDao {
         return new QueryHelper<List<AchievementEntity>>() {
             @Override
             protected void executeQuery(Statement statement, Connection connection) throws SQLException {
-                List<AchievementEntity> achievementEntityList = new LinkedList<>();
+                List<AchievementEntity> achievementEntityList = new ArrayList<>();
                 ResultSet resultSet = statement.executeQuery("select * from Achievement");
                 while (resultSet.next()) {
                     AchievementEntity achievementEntity = new AchievementEntity();
-                    achievementEntity.setId(resultSet.getInt(1));
-                    achievementEntity.setName(resultSet.getString(2));
-                    achievementEntity.setDescription(resultSet.getString(3));
+                    achievementEntity.setId(resultSet.getInt("id"));
+                    achievementEntity.setName(resultSet.getString("name"));
+                    achievementEntity.setDescription(resultSet.getString("description"));
                     achievementEntityList.add(achievementEntity);
                 }
                 returnResult(achievementEntityList);
---
+++ src/main/java/game/service/AchievementServise.java
@@ -0,0 +1,5 @@
+package game.service;
+
+public interface AchievementServise {
+
+}
