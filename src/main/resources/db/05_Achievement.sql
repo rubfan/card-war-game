2f0ca36f57def971b65b6ee58b42d4d40dd5751b
Alexey Kostash <odessakost@gmail.com>
2018-09-02 15:16:39 +0300

    update Achievement.sql

--- src/main/resources/db/05_Achievement.sql
+++ src/main/resources/db/05_Achievement.sql
@@ -1,7 +1,7 @@
 INSERT INTO `Achievement`
-    (`name`,`description`)
+    (`id`, `name`,`description`)
 VALUES
-    ('Let us make bourbone', 'You have got 10000 corn'),
-    ('Chicken Town','You have built 10 Chicken Houses'),
-    ('Granary Fan','You have upgraded Granary 5 times'),
-    ('I am ready for the fifth labour of Heracles','You have got 5 Chicken Houses and 1000 dung');
+    (1, 'Let us make bourbone', 'You have got 10000 corn'),
+    (2, 'Chicken Town','You have built 10 Chicken Houses'),
+    (3, 'Granary Fan','You have upgraded Granary 5 times'),
+    (4, 'I am ready for the fifth labour of Heracles','You have got 5 Chicken Houses and 1000 dung');
