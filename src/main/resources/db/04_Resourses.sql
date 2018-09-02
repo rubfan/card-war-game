cef9ac078a93249931cd6572e5116ad4b3b72250
Alexey Kostash <odessakost@gmail.com>
2018-09-02 15:22:51 +0300

    fill some tables

---
+++ src/main/resources/db/04_Resourses.sql
@@ -0,0 +1,27 @@
+INSERT INTO `Resource`
+    (`id`,`name`,`description`)
+VALUES
+    (1,'Corn','Cocks eat corn,really!'),
+    (2,'Eggs','The new generations coming.'),
+    (3,'Chickens','Children are flowers of life'),
+    (4,'Dung','Just chicken shit'),
+    (5,'Hens','Oh, ladies!'),
+    (6,'Cocks','The main army reseve'),
+    (7,'Fighting Cocks','fighting cocks'),
+    (8,'Stone','I am rock!'),
+    (9,'Cocks with hammers','Slightly improved soldier'),
+    (10,'Cocks with slingshots','Not the hobbits, but deadly'),
+    (11,'Steel','Forge steel while its hot'),
+    (12,'Cocks swordsmen (in lats)','Armored cocknight. Beware.'),
+    (13,'Cocks with axes (in lats)','Iron lumberjack, where are u?'),
+    (14,'Wood','Simple tree log.'),
+    (15,'Cocks archers','Longbow equipped cock. Really?'),
+    (16,'Cocks lancers','Just give us spears!'),
+    (17,'Gunpowder','Dont bring fire too close.'),
+    (18,'Cocks gunners','Im got a gun!'),
+    (19,'Cocks with bazookas','Just point and click, baby.'),
+    (20,'Mana','The mysterious magic power.'),
+    (21,'Cocks magicians"','They can turn a lead in a gold. I believe in it.'),
+    (22,'Cocks prophets','Marty, i saw the future!'),
+    (23,'Experience','Need to advance, as always.'),
+    (24,'Cockiness','Listen, why are you so cocky?');
