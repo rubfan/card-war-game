7fc08f4082cb590ab71da274bd109dccafe9a0bd
Alexey Kostash <odessakost@gmail.com>
2018-09-04 10:12:34 +0300

    update AccountAchievementDao

--- src/main/java/game/repository/dao/AccountAchievementDao.java
+++ src/main/java/game/repository/dao/AccountAchievementDao.java
@@ -7,5 +7,5 @@ import java.util.List;
 public interface AccountAchievementDao {
     List<AccountAchievementEntity> getAccountAchievementList(Integer accountId);
     void deleteAccountAchievementList(Integer accountId);
-    void addAchievementToAccountAchievementList(Integer accountId, Integer achievementId);
+    void updateAccountAchievementList(Integer accountId);
 }
