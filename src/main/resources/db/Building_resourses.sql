ec3c32f8dbf0c94046600f15d7d4c0d865428e5c
AndrewChern <nrnk3373@gmail.com>
2018-09-04 20:56:00 +0300

    First commit in BuildingController

---
+++ src/main/resources/db/Building_resourses.sql
@@ -0,0 +1,8 @@
+INSERT INTO `Building_Resource`
+    (`id`,`building_id`,`resource_id`,`number_per_sec`)
+VALUES
+    (1,1,1,150),
+    (2,2,2,100),
+    (3,3,3,50),
+    (4,4,4,50),
+    (5,5,5,50);
