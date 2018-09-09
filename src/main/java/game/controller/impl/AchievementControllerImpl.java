aca57906922e3d69baa13081323f4e228c241ca7
Alexey Kostash <odessakost@gmail.com>
2018-09-09 17:33:23 +0300

    update AchievementControllerImpl

--- src/main/java/game/controller/impl/AchievementControllerImpl.java
+++ src/main/java/game/controller/impl/AchievementControllerImpl.java
@@ -5,10 +5,8 @@ import game.dto.AchievementDto;
 import game.service.AchievementService;
 
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 /**
@@ -24,7 +22,6 @@ public class AchievementControllerImpl implements AchievementController {
     @Override
     @GET
     @Path("/list")
-    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public List<AchievementDto> getAllAchievementList() {
         return achievementService.getAllAchievementList();
     }
